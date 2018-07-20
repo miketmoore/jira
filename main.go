94e1c10a5f8757002ffb0fd045c8a3aed41820fd
Michael Moore <michael.moore@contrastsecurity.com>
2018-07-20 07:16:34 -0400

    :ok_hand: check error from json unmarshal

--- main.go
+++ main.go
@@ -51,12 +51,14 @@ func main() {
 
 	file, err := ioutil.ReadFile(configpath)
 	if err != nil {
-		fmt.Println("Failed to load config")
-		fmt.Println(err)
+		fmt.Printf("Failed to get issue: %s", err)
 		os.Exit(1)
 	}
 
-	json.Unmarshal(file, &config)
+	err = json.Unmarshal(file, &config)
+	if err != nil {
+		fmt.Printf("Failed to unmarshal JSON: %s", err)
+	}
 
 	issueURL := fmt.Sprintf("URL: %s/browse/%s", config.BaseURL, *issueID)
 
